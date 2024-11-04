31df8927b40c8036150cc7a4dd398e8c88872ae2
Devmdmainuddin <devmdmainuddin@gmail.com>
2024-11-04 20:30:48 +0600

    project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+ 
+
+  return (
+    <>
+     <Button>check</Button>
+    </>
+  )
+}
+
+export default App
---
+++ server/server.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+const express=require("express");
+const cors= require("cors");
+const mongoose=require("mongoose");
+
+
+const app= express();
+const PORT =process.env.PORT || 5000;
+const MONGO_URL = process.env.MONGO_URL;
+
+cors({
+    origin: process.env.CLIENT_URL,
+    methods:['GET', 'POST', 'DELETE','PUT','PATH'],
+    allowedHeaders:['Content-Type', 'Authorization',]
+    
+})
+
+app.use(express.json());
+
+// Connect to MongoDB
+// {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
+mongoose.connect(MONGO_URL, )
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+
+app.use((err,req,res,next)=>{
+    console.log(err.stack);
+    res.status(500).send({
+        success:false,
+        message: 'Something went wrong'
+    })
+})
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`)
+})
+
+// const corsOptions ={
+//     origin: process.env.CLIENT_URL,
+//     credentials: true
+// }
+
+
+// mongodb+srv://<db_username>:<db_password>@cluster0.sk1ew0y.mongodb.net/?retryWrites=true&w=majority&appName=cluster0
