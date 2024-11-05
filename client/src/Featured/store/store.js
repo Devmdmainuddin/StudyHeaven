a79db70930976f07373efaeefda25f58259d2d68
Devmdmainuddin <devmdmainuddin@gmail.com>
2024-11-05 09:29:48 +0600

    redux add

---
+++ client/src/Featured/store/store.js
@@ -0,0 +1,16 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { authApi } from "../Auth/authApi";
+
+export const store = configureStore({
+  reducer: {
+    [authApi.reducerPath]: authApi.reducer,
+
+  },
+  
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(authApi.middleware),
+
+});
+
+setupListeners(store.dispatch); // 
